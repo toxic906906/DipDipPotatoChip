ebe5fd9a863d461bf5886de0e6b79f4d3116d126
toxic906906 <scopes.bobby@gmail.com>
2015-12-29 01:01:07 -0500

    Commit

---
+++ src/com/odin/warwick/Test.java
@@ -0,0 +1,13 @@
+package com.odin.warwick;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test extends JavaPlugin {
+    // Called when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Called when plugin is disabled
+    @Override
+    public void onDisable() {
+    }
+}
