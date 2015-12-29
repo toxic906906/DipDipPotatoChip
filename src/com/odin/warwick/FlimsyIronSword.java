e72242f70fec429a27f1e460771b1cfeb26eff31
toxic906906 <scopes.bobby@gmail.com>
2015-12-29 01:31:33 -0500

    Fixed Buggy Code

--- src/com/odin/warwick/FlimsyIronSword.java
+++ src/com/odin/warwick/FlimsyIronSword.java
@@ -6,19 +6,19 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FlimsyIronSword extends JavaPlugin{
+public class FlimsyIronSword extends JavaPlugin
+{
+
     @Override
-    public void onEnable(){
-    	
+    public void onEnable()
+    {
         ItemStack sword = new ItemStack( Material.DIAMOND_SWORD );
         ItemMeta im = sword.getItemMeta();
         im.setDisplayName( "Flimsy Iron Sword" );
         sword.setItemMeta( im );
         sword.addEnchantment( Enchantment.DAMAGE_ALL, 1 );
         ShapedRecipe recipe = new ShapedRecipe( sword );
-        recipe.shape( "", "", " I " );
-        recipe.shape( "", " I ", "" );
-        recipe.shape( " S ", "", "" );
+        recipe.shape( " I ", " I ", " S " );
         recipe.setIngredient( 'I', Material.IRON_INGOT );
         recipe.setIngredient( 'S', Material.STICK );
         getServer().addRecipe( recipe );
